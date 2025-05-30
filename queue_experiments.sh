c987138acc1f37ed812fb592270dc8855e6c5453
Mohammad Amin Rezaei Sepehr <mohammadaminrez@gmail.com>
2025-05-30 09:55:41 +0200

    refactor: rewrite queue_experiments

--- queue_experiments.sh
+++ queue_experiments.sh
@@ -1,7 +1,7 @@
 #/bin/sh
 
-for D in 1 2 5 10; do
- for LAMBD in 0.5 0.9 0.95 0.99; do
+for LAMBD in 0.5 0.9 0.95 0.99; do
+ for D in 1 2 5 10; do
   echo $LAMBD $D
   ./queue_sim.py --lambd $LAMBD --d $D --n 10 --csv out.csv --max-t 100_000
  done
